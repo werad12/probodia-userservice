c55d8f7c0c822661945d006bf980c76d1ece7922
mina881@naver.com <tjwlsdl1!>
2022-09-09 01:18:14 +0900

    user entity 추가 + meal, medicine update 수정 + user 조회, 수정 메서드 보완

--- src/main/java/com/probodia/userservice/api/service/user/UserService.java
+++ src/main/java/com/probodia/userservice/api/service/user/UserService.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -102,6 +103,7 @@ public class UserService {
                 .build();
     }
 
+    @Transactional
     public UserInfoVO updateUserInfo(UserInfoRequestVO request, User user) {
 
         if(request.getHeight()!=null){
